83d65a21dc98d9093cacc8e7a62d46c5f98353d3
Rxzh <54960213+Rxzh@users.noreply.github.com>
2023-01-03 16:03:31 +0100

    rename front2 to front

--- front.py
+++ front.py
@@ -3,6 +3,15 @@ from tkinter import ttk
 import asyncio
 import os
 from time import sleep
+import sys
+
+
+if getattr(sys, 'frozen', False):
+    # frozen
+    #os.chdir(sys._MEIPASS)
+    os.chdir(os.path.dirname(sys.executable))
+
+
 
 class App:
     async def exec(self):
@@ -11,55 +20,71 @@ class App:
 
 
 class Window(tk.Tk):
-    def __init__(self, loop):        
-
+    def __init__(self, loop):
         self.loop = loop
         self.root = tk.Tk()
+        self.app_closing = False
+        self.stop_script = True
+
+        #couleur jaune
+        self.root.config(bg='#FFFF00')
+
         self.root.title("MeyzhBOT")
         self.root.geometry("260x260")
         self.script_choice = tk.StringVar(self.root)
         self.script_choice.set("exp") # default value
-
         scripts = [s.split('/')[-1] for s in os.listdir('scripts') if os.path.isdir(f'scripts/{s}') and s != '__pycache__']
-
-        #scripts = ["Script 1", "Script 2", "Script 3"]
         self.script_menu = tk.OptionMenu(self.root, self.script_choice, *scripts)
         self.script_menu.pack()
 
-        # Create a button to launch the script
-        self.launch_button = tk.Button(text="Launch", command=lambda: self.loop.create_task(self.run_script()))
-        self.launch_button.pack()
+        #button_block = tk.Button(text="Async func", width=10, command=lambda: self.loop.create_task(self.async_func()))
+        #button_block.pack()
 
-        # Create a button to close the window
-        self.quit_button = tk.Button(text="Quit", command=lambda: self.loop.create_task(self.close_window()))
-        self.quit_button.pack()
+        s = ttk.Style()
+        s.configure('TButton', bg='#FFFF00')
 
+        button_script = tk.Button(text="Launch script", width=10, command=lambda: self.loop.create_task(self.launch_script()))
+        button_script.pack()        
+
+        button_strop_script = tk.Button(text="Stop script", width=10, command=lambda: self.loop.create_task(self.stop_script_func()))        
+        button_strop_script.pack()
+
+        button_quit = tk.Button(text="Quit", width=10, command=lambda: self.loop.create_task(self.quit()))
+        button_quit.pack()
         
-        
-
-
-    async def close_window(self):
-        self.root.destroy()
-        await asyncio.sleep(.1)
-
-    async def run_script(self):
-        # Get the selected script from the dropdown menu
-        selected_script = self.script_choice.get()
-        # Write code to run the selected script here
-
-        os.system(f"python scripts/{selected_script}/{selected_script}.py")
-        await print(f"Running script: {selected_script}")
-        
-        
-
-    #async def show(self):
-    #    self.root.mainloop()
-    #    await asyncio.sleep(0)
 
     async def show(self):
-        while True:
+        while not(self.app_closing):
+
             self.root.update()
             await asyncio.sleep(.1)
 
-asyncio.run(App().exec())
 
+    async def quit(self):
+        self.root.destroy()
+        self.app_closing = True
+    
+    async def async_func(self):
+        for _ in range(10):
+            print('coucou')
+            await asyncio.sleep(1)
+    
+    async def launch_script(self):
+        selected_script = self.script_choice.get()
+        
+
+        #TODO import everything before to fit frozen app
+        package = "scripts.{}.{}".format(selected_script, selected_script)
+        name = "main"
+        main = getattr(__import__(package, fromlist=[name]), name)
+
+        self.stop_script = False
+        await main(self)
+
+    async def stop_script_func(self):
+        self.stop_script = True
+
+
+    
+if __name__ == "__main__":
+    asyncio.run(App().exec())
